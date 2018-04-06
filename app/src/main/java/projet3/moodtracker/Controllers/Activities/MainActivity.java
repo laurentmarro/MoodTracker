85911259d11baaba41694de774090a746b6a5fa7
laurentmarro <32217159+laurentmarro@users.noreply.github.com>
2018-04-06 19:17:26 +0200

    ViewPager configuration in the MainActivity
    Problem still the same for position

--- app/src/main/java/projet3/moodtracker/Controllers/Activities/MainActivity.java
+++ app/src/main/java/projet3/moodtracker/Controllers/Activities/MainActivity.java
@@ -8,34 +8,11 @@ import projet3.moodtracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static int position = 3;
-
-    // Getter et setter
-
-    public static int getPosition() {
-        return position;
-    }
-
-    public static void setPosition(int position) {
-        MainActivity.position = position;
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Configure ViewPager
-
-        this.configureViewPager();
-    }
-
-    private void configureViewPager(){
-
-        // Get ViewPager from layout
-        ViewPager pager = (ViewPager)findViewById(R.id.activity_main_viewpager);
-        // Set Adapter PageAdapter and glue it together
-        pager.setAdapter(new PageAdapter(getSupportFragmentManager()) {
-        });
+        ViewPager pager = (ViewPager) findViewById(R.id.activity_main_viewpager);
+        pager.setAdapter(new PageAdapter(getSupportFragmentManager()) {});
     }
 }
